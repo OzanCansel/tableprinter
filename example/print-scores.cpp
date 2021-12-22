262596d076cd4ac55d8ae8365885932e89508189
OzanCansel <ozancansel@tutanota.com>
2021-12-22 19:22:01 +0300

    hex {} , decimal {} usages included in print-scores

--- example/print-scores.cpp
+++ example/print-scores.cpp
@@ -11,10 +11,10 @@ int main()
     printer p
     {
         {
-            { name { "id" }      , width { 4 }  } ,
-            { name { "name" }    , width { 10 } } ,
+            { name { "id" }      , width { 4 }  , hex {} } ,
+            { name { "name" }    , width { 8 } } ,
             { name { "surname" } , width { 10 } } ,
-            { name { "rank" }    , width { 6 }  } ,
+            { name { "rank" }    , width { 6 } , decimal {} } ,
             { name { "score" }   , width { 7 } , fixed { } , precision { 2 } }
         } ,
         { std::cout , scores_f }
@@ -23,12 +23,12 @@ int main()
     p.sanity_check()
      .echo( "The scores are listed below with their ranks :" )
      .print_headers()
-     .print( 1 , "Lucy"   , "Ballmer"  , 2 , 94.13 )
-     .print( 2 , "Roger"  , "Bacon"    , 5 , 77.13 )
-     .print( 3 , "Anna"   , "Smith"    , 3 , 87.13 )
-     .print( 4 , "Robert" , "Schwartz" , 1 , 98.34 )
-     .print( 5 , "Robert" , "Brown"    , 4 , 84.34 )
-     .print( std::make_tuple( 6 , "David" , "Timothy" , 6 , 71.34 ) );
+     .print( 5000 , "Lucy"   , "Ballmer"  , 2 , 94.13 )
+     .print( 5100 , "Roger"  , "Bacon"    , 5 , 77.13 )
+     .print( 5200 , "Anna"   , "Smith"    , 3 , 87.13 )
+     .print( 5300 , "Robert" , "Schwartz" , 1 , 98.34 )
+     .print( 5400 , "Robert" , "Brown"    , 4 , 84.34 )
+     .print( std::make_tuple( 5500 , "David" , "Timothy" , 6 , 71.34 ) );
 
     /*
        # Run the command if you want to see maximum score
